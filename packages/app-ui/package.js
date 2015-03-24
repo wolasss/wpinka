fab1ab7a2d527a056e5b769137e88ba53f1ce6ef
Adam Wolski <wolas.abc@gmail.com>
2015-03-24 22:46:47 +0100

    WPIN-30 Add sidebar menu

--- packages/app-ui/package.js
+++ packages/app-ui/package.js
@@ -6,9 +6,13 @@ Package.on_use(function (api) {
     var both = ['client', 'server'];
     api.use(['app', 'fourseven:scss'], both);
 
+    api.add_files('client/styles/_variables.scss', 'client');
     api.add_files('client/styles/main.scss', 'client');
     api.add_files('client/styles/login.scss', 'client');
     api.add_files('client/styles/alerts.scss', 'client');
+    api.add_files('client/styles/bar.scss', 'client');
+    api.add_files('client/styles/menu.scss', 'client');
+    api.add_files('client/styles/list.scss', 'client');
 });
 
 Package.on_test(function (api) {
