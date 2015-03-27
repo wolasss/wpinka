d198a88233fd40771a33092c05425032881a9cfd
Łukasz Wojtczak <abditus113@gmail.com>
2015-03-27 21:14:16 +0100

    WPIN-33 provide translations for register module

--- packages/app-register/package.js
+++ packages/app-register/package.js
@@ -4,10 +4,12 @@ Package.describe({
 
 Package.on_use(function (api) {
     var both = ['client', 'server'];
-    api.use(['app', 'templating', 'underscore', 'app-validators', 'accounts-facebook', 'accounts-password'], both);
+    api.use(['app', 'templating', 'underscore', 'app-validators', 'accounts-facebook', 'accounts-password', 'tap:i18n'], both);
     api.use(['wolas:alerts', 'bootstrap-alerts', 'iron:router'], 'client');
 
     api.export && api.export("APP");
+
+    api.add_files('package-tap.i18n', both);
 
     api.add_files('client/views/register_screen.html', 'client');
     api.add_files('client/views/login_services.html', 'client');
@@ -26,6 +28,11 @@ Package.on_use(function (api) {
 
     api.add_files('client/views/events/register_screen.js', 'client');
     api.add_files('client/views/events/login_services.js', 'client');
+
+    api.add_files([
+        'locales/en.i18n.json',
+        'locales/pl.i18n.json'
+    ], both);
 });
 
 Package.on_test(function (api) {
