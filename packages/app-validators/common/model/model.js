454bc9e7b8b1b653d4a657ff49519b719250dd30
Łukasz Wojtczak <abditus113@gmail.com>
2015-04-04 15:25:56 +0200

    WPIN-24 I use braces

--- packages/app-validators/common/model/model.js
+++ packages/app-validators/common/model/model.js
@@ -5,7 +5,6 @@ _.extend(APP.Validators, {
 		var ret = false;
 
 		if(typeof(email) == "string" && email) {
-
 			var re = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/mig;
 			ret = re.test(email);
 		}
@@ -13,18 +12,21 @@ _.extend(APP.Validators, {
 		return ret;
 	},
 	isEmptyString: function(content){
-		if(typeof content == "string" && content.length == 0)
+		if(typeof content == "string" && content.length == 0){
 			return true;
+		}
 		return false;
 	},
 	isContentTooShort: function(content){
-		if(typeof content == "string" && content.length < 10)
+		if(typeof content == "string" && content.length < 10){
 			return true;
+		}
 		return false;
 	},
 	isContentTooLong: function(content){
-		if(typeof content == "string" && content.length > 512)
+		if(typeof content == "string" && content.length > 512){
 			return true;
+		}
 		return false;
 	}
 });
