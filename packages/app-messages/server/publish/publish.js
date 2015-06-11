3ca3d800ef5fcd6cebf294807290474f130114bd
Łukasz Wojtczak <wojtczakf@gmail.com>
2015-06-11 13:44:10 +0200

    check against undefined thread

--- packages/app-messages/server/publish/publish.js
+++ packages/app-messages/server/publish/publish.js
@@ -1,7 +1,7 @@
 Meteor.publish('messages', function(threadId){
 	check(threadId, String);
 	var thread = Mongo.Collection.get('threads').findOne(threadId);
-	if(_.indexOf(thread.participants, this.userId) > -1){
+	if(thread && _.indexOf(thread.participants, this.userId) > -1){
 		return APP.Messages.collection.find({ threadId: threadId });
 	} else {
 		return [];
